317b8074939e52396dacdaa6d58728d284fb4518
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-06-04 11:34:33 -0400

    - adding a map

--- your-code.js
+++ your-code.js
@@ -1,4 +1,4 @@
-angular.module('jsCodingTest', ['cpLib']);
+angular.module('jsCodingTest', ['cpLib', 'ngMap']);
 
 angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function(PackagesFactory, $scope) {
     // Your JavaScript goes here.
@@ -6,6 +6,14 @@ angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function(
     // for burritos that can be delivered to the Houses of Parliament in London.
     // The API URL that should be called is:
     // https://api.citypantry.com/packages/search?name=Burrito&postcode=SW1A%200AA
+    $scope.vendorCenter = function(vendor) {
+        return vendor.addresses[0].latitude + ", " + vendor.addresses[0].longitude;
+    };
+    $scope.addressPosition = function(address) {
+        console.log("[" + address.latitude + ", " + address.longitude + "]");
+        return "[" + address.latitude + ", " + address.longitude + "]";
+    };
+
     $scope.loading = true;
     $scope.packages = [];
     PackagesFactory.searchPackages('Burrito', 'SW1A%200AA').success(function(data) {
@@ -14,7 +22,7 @@ angular.module('jsCodingTest').controller('GiveTheGovernmentABurrito', function(
         var p;
         for (var i in data.packages) {
             p = data.packages[i];
-            console.log(p.vendor.facebookUrl);
+            console.log(p.vendor.addresses);
         }
     });
 });
